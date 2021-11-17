dc90420a43de725ff21e13a21bf7bfad1ec4c25d
dandanloveJM <gigi834921748@qq.com>
2021-11-17 22:26:27 +0800

    修改mybatis配置

---
+++ src/main/resources/db/migration/V3__Insert_blog.sql
@@ -0,0 +1,4 @@
+insert into blog (id, user_id, title, description, content, updated_at, created_at)
+values (1,1,'title1','des', 'content1',now(),now());
+insert into blog (id, user_id, title, description, content, updated_at, created_at)
+values (2,2,'title2', 'des2', 'content2',now()+1000,now()+1000);
