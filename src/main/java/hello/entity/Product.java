98400cf116346621c54a58776a6d5bb815eb8e22
dandanloveJM <gigi834921748@qq.com>
2022-01-10 10:50:33 +0800

    增加产值和项目实体

---
+++ src/main/java/hello/entity/Product.java
@@ -0,0 +1,51 @@
+package hello.entity;
+
+import java.math.BigDecimal;
+
+public class Product {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProcessId() {
+        return processId;
+    }
+
+    public void setProcessId(String processId) {
+        this.processId = processId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(BigDecimal percentage) {
+        this.percentage = percentage;
+    }
+
+    public Integer getProduct() {
+        return product;
+    }
+
+    public void setProduct(Integer product) {
+        this.product = product;
+    }
+
+    private Integer id;
+    private String processId;
+    private Integer userId;
+    private BigDecimal percentage;
+    private Integer product;
+}
---
+++ src/main/java/hello/entity/Project.java
@@ -0,0 +1,60 @@
+package hello.entity;
+
+public class Project {
+    private Integer id;
+    private String processId;
+    private String name;
+    private String number;
+    private String type;
+    private String attachment;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProcessId() {
+        return processId;
+    }
+
+    public void setProcessId(String processId) {
+        this.processId = processId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
+
+
+}
