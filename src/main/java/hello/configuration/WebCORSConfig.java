c1491a6c0f0f9a9cf27232f6a1b2bb8356948d29
dandanloveJM <gigi834921748@qq.com>
2022-01-16 21:23:42 +0800

    配置CORS

---
+++ src/main/java/hello/configuration/WebCORSConfig.java
@@ -0,0 +1,20 @@
+package hello.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class WebCORSConfig implements WebMvcConfigurer {
+    @Override
+    public  void  addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(30*1000);
+    }
+}
--- src/main/java/hello/configuration/WebSecurityConfig.java
+++ src/main/java/hello/configuration/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 @EnableWebSecurity
@@ -29,4 +30,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager customAuthenticationManager() throws Exception {
         return authenticationManager();
     }
+
+
 }
