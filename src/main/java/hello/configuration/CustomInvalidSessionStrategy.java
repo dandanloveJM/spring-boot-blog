8fdba90e21f2051c1394f5cb84d5b24ff2a93d82
dandanloveJM <gigi834921748@qq.com>
2022-01-23 12:40:39 +0800

    session过期后，response.addCookie,设置cookie的Max-Age为0，实现浏览器端删除过期cookie

--- src/main/java/hello/configuration/CustomInvalidSessionStrategy.java
+++ src/main/java/hello/configuration/CustomInvalidSessionStrategy.java
@@ -1,5 +1,7 @@
 package hello.configuration;
 
+import com.alibaba.fastjson.JSON;
+import hello.entity.UserResult;
 import org.springframework.security.web.session.InvalidSessionStrategy;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +15,17 @@ import java.io.IOException;
 public class CustomInvalidSessionStrategy implements InvalidSessionStrategy {
     @Override
     public void onInvalidSessionDetected(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
+        System.out.println("没有过期吗");
+        UserResult result = UserResult.failure("expire", "expire");
+
         Cookie cookie = new Cookie("JSESSIONID", null);
         cookie.setPath("/");
-        cookie.setMaxAge(0);
+        cookie.setMaxAge(0);  // 设置为0，浏览器端就会删除同域名，同path的cookie
+        cookie.setDomain("localhost");
+        httpServletResponse.addCookie(cookie);
+
         httpServletResponse.setContentType("application/json;charset=utf-8");
-        httpServletResponse.setStatus(-401);
+        httpServletResponse.getWriter().write(JSON.toJSONString(result));
 
     }
 }
--- src/main/java/hello/configuration/WebSecurityConfig.java
+++ src/main/java/hello/configuration/WebSecurityConfig.java
@@ -14,16 +14,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private CustomInvalidSessionStrategy invalidSessionStrategy;
-
-    @Autowired
-    private WebCORSConfig webCORSConfig;
-
-    @Autowired
-    private LoginAuthenticationProvider loginAuthenticationProvider;
-
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,12 +21,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers("/start").authenticated();
-//                .and()
-//                .authenticationProvider(loginAuthenticationProvider)
-                http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                .invalidSessionStrategy(invalidSessionStrategy).and()
-                .cors();
+
+        http.cors();
+        http.sessionManagement(session -> session.invalidSessionStrategy(new CustomInvalidSessionStrategy()));
+
+
 //                .antMatchers("/r4/approveTask").access("hasAuthority('ROLE_R4审核')");
 
     }
