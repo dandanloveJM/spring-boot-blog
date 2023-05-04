737195216eb02939d1d2324bcbda03f9272b251c
dandanloveJM <gigi834921748@qq.com>
2023-05-04 22:17:26 +0800

    注释二次分配产值

--- src/main/java/hello/service/ReallocateBonusService.java
+++ src/main/java/hello/service/ReallocateBonusService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -37,83 +38,97 @@ public class ReallocateBonusService {
             // 把个人的产值更新
             productDao.insertProducts(productList);
             // User和Product Map
-            HashMap<Integer, BigDecimal> userReallocateProductMap = new HashMap<>();
-
-            for (Product product : productList) {
-                userReallocateProductMap.put(product.getUserId(), product.getProduct());
-            }
-
-
-            List<Integer> userIds = productList.stream().map(Product::getUserId).collect(Collectors.toList());
-
-            // 找出团队一共增加的产值， 就是需要减去的Bonus
-            UserListResult result = userDao.getTeamByUserIds(userIds);
-            if (Objects.equals(result.getStatus(), "fail")) {
-                return ProductResult.failure("查询失败");
-            } else {
-                List<User> usersWithTeamId = result.getData();
-                List<User> usersWithTeamsAndProducts = usersWithTeamId.stream()
-                        .peek(item -> item.setUserReallocateProduct(userReallocateProductMap.get(item.getId()))).collect(Collectors.toList());
-                Map<String, BigDecimal> teamProductsMap = new HashMap<>();
-
-
-                for (User user : usersWithTeamsAndProducts) {
-                    String team = user.getDepartment();
-                    if (teamProductsMap.containsKey(team)) {
-                        teamProductsMap.put(team, teamProductsMap.get(team).add(user.getUserReallocateProduct()));
-                    } else {
-                        teamProductsMap.put(team, user.getUserReallocateProduct());
-                    }
-                }
-
-                // 把每个TEAM的产值记录变成负的product, 准备写进product表
-                List<Product> teamProductIntoTable = new ArrayList<>();
-                // 每个TEAM扣分账号userId的MAP
-                Map<String, List<String>> teamIdMap = new HashMap<>();
-                teamIdMap.put("Z1", Arrays.asList("94","支持一室扣产值专用"));
-                teamIdMap.put("Z2",Arrays.asList("95","支持二室扣产值专用"));
-                teamIdMap.put("F1", Arrays.asList("96","方法一室扣产值专用"));
-                teamIdMap.put("F2", Arrays.asList("97","方法二室扣产值专用"));
-
-                String processId = "reallocate--bonus" + RandomStringUtils.randomAlphanumeric(5);
-                for (var entry : teamProductsMap.entrySet()) {
-                    Product product = new Product();
-                    Integer teamId = Integer.valueOf(teamIdMap.get(entry.getKey()).get(0));
-                    String teamName = teamIdMap.get(entry.getKey()).get(1);
-                    product.setProcessId(processId);
-                    product.setDisplayName(teamName);
-                    product.setBonus(entry.getValue().negate());
-                    product.setUserId(teamId);
-                    teamProductIntoTable.add(product);
-                }
-
-                productDao.insertProducts(teamProductIntoTable);
-
-                List<TeamRank> teamBonusList = new ArrayList<>();
-                if (bonusType.equals("R4")) {
-                    for (var entry : teamProductsMap.entrySet()) {
-                        TeamRank tmp = new TeamRank();
-                        tmp.setTeam(entry.getKey());
-                        tmp.setR4Bonus(entry.getValue());
-
-                        teamBonusList.add(tmp);
-                    }
-                    teamBonusDao.updateTeamBonusByAdminR4(teamBonusList);
-
-                } else if (bonusType.equals("R5")) {
-                    for (var entry : teamProductsMap.entrySet()) {
-                        TeamRank tmp = new TeamRank();
-                        tmp.setTeam(entry.getKey());
-                        tmp.setR5Bonus(entry.getValue());
-
-                        teamBonusList.add(tmp);
-                    }
-                    teamBonusDao.updateTeamBonusByAdminR5(teamBonusList);
-                }
+//            HashMap<Integer, BigDecimal> userReallocateProductMap = new HashMap<>();
+//
+//            for (Product product : productList) {
+//                userReallocateProductMap.put(product.getUserId(), product.getProduct());
+//            }
+//
+//
+//            List<Integer> userIds = productList.stream().map(Product::getUserId).collect(Collectors.toList());
+//
+//            // 找出团队一共增加的产值， 就是需要减去的Bonus
+//            UserListResult result = userDao.getTeamByUserIds(userIds);
+//            if (Objects.equals(result.getStatus(), "fail")) {
+//                return ProductResult.failure("查询失败");
+//            } else {
+//                List<User> usersWithTeamId = result.getData();
+//                List<User> usersWithTeamsAndProducts = usersWithTeamId.stream()
+//                        .peek(item -> item.setUserReallocateProduct(userReallocateProductMap.get(item.getId()))).collect(Collectors.toList());
+//                Map<String, BigDecimal> teamProductsMap = new HashMap<>();
+//
+//
+//                for (User user : usersWithTeamsAndProducts) {
+//                    String team = user.getDepartment();
+//
+//                    BigDecimal divisor = new BigDecimal(2);
+//
+//                    if(team.equals("Z")) {
+//                        teamProductsMap.put("Z1", user.getUserReallocateProduct().divide(divisor, RoundingMode.CEILING));
+//                        teamProductsMap.put("Z2", user.getUserReallocateProduct().divide(divisor, RoundingMode.CEILING));
+//                    } else if (team.equals("F")){
+//                        teamProductsMap.put("F1", user.getUserReallocateProduct().divide(divisor, RoundingMode.CEILING));
+//                        teamProductsMap.put("F2", user.getUserReallocateProduct().divide(divisor, RoundingMode.CEILING));
+//                    } else {
+//                        if (teamProductsMap.containsKey(team)) {
+//                            teamProductsMap.put(team, teamProductsMap.get(team).add(user.getUserReallocateProduct()));
+//                        } else {
+//                            teamProductsMap.put(team, user.getUserReallocateProduct());
+//                        }
+//
+//                    }
+//
+//
+//                }
+//
+//                // 把每个TEAM的产值记录变成负的product, 准备写进product表
+//                List<Product> teamProductIntoTable = new ArrayList<>();
+//                // 每个TEAM扣分账号userId的MAP
+//                Map<String, List<String>> teamIdMap = new HashMap<>();
+//                teamIdMap.put("Z1", Arrays.asList("94","支持一室扣产值专用"));
+//                teamIdMap.put("Z2",Arrays.asList("95","支持二室扣产值专用"));
+//                teamIdMap.put("F1", Arrays.asList("96","方法一室扣产值专用"));
+//                teamIdMap.put("F2", Arrays.asList("97","方法二室扣产值专用"));
+//
+//                String processId = "reallocate--bonus" + RandomStringUtils.randomAlphanumeric(5);
+//                for (var entry : teamProductsMap.entrySet()) {
+//                    Product product = new Product();
+//                    Integer teamId = Integer.valueOf(teamIdMap.get(entry.getKey()).get(0));
+//                    String teamName = teamIdMap.get(entry.getKey()).get(1);
+//                    product.setProcessId(processId);
+//                    product.setDisplayName(teamName);
+//                    product.setBonus(entry.getValue().negate());
+//                    product.setUserId(teamId);
+//                    teamProductIntoTable.add(product);
+//                }
+//
+//                productDao.insertProducts(teamProductIntoTable);
+//
+//                List<TeamRank> teamBonusList = new ArrayList<>();
+//                if (bonusType.equals("R4")) {
+//                    for (var entry : teamProductsMap.entrySet()) {
+//                        TeamRank tmp = new TeamRank();
+//                        tmp.setTeam(entry.getKey());
+//                        tmp.setR4Bonus(entry.getValue());
+//
+//                        teamBonusList.add(tmp);
+//                    }
+//                    teamBonusDao.updateTeamBonusByAdminR4(teamBonusList);
+//
+//                } else if (bonusType.equals("R5")) {
+//                    for (var entry : teamProductsMap.entrySet()) {
+//                        TeamRank tmp = new TeamRank();
+//                        tmp.setTeam(entry.getKey());
+//                        tmp.setR5Bonus(entry.getValue());
+//
+//                        teamBonusList.add(tmp);
+//                    }
+//                    teamBonusDao.updateTeamBonusByAdminR5(teamBonusList);
+//                }
 
                 return ProductResult.success("二次分配产值成功");
 
-            }
+
 
         } catch (Exception e) {
             return ProductResult.failure("程序异常");
